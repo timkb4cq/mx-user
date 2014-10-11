c0fb99fe533ce5a59cbc1bcf990915268ddba590
Adrian <adrian@mxlinux.org>
2014-10-11 16:36:19 -0400

    changed version format

--- mconfig.cpp
+++ mconfig.cpp
@@ -889,7 +889,7 @@ void MConfig::executeChild(const char* cmd, const char* param)
 void MConfig::on_buttonAbout_clicked() {
     QMessageBox msgBox(QMessageBox::NoIcon,
                        tr("About MX User Manager"), "<p align=\"center\"><b><h2>" +
-                       tr("MX User Manager") + "</h2></b></p><p align=\"center\">MX14+git20140728</p><p align=\"center\"><h3>" +
+                       tr("MX User Manager") + "</h2></b></p><p align=\"center\">MX14+git20141011</p><p align=\"center\"><h3>" +
                        tr("Simple user configuration for antiX MX") + "</h3></p><p align=\"center\"><a href=\"http://www.mepiscommunity.org/mx\">http://www.mepiscommunity.org/mx</a><br /></p><p align=\"center\">" +
                        tr("Copyright (c) antiX") + "<br /><br /></p>", 0, this);
     msgBox.addButton(tr("License"), QMessageBox::AcceptRole);
